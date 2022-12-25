92f13e1beb19a9d9ea1a81f5334814da36bdc127
senthu macbook <senthuranselvanathanbusiness@gmail.com>
2022-12-25 16:11:15 +0100

    init

---
+++ src/main/java/com/skeleton/springsecurityskeleton/SpringSecuritySkeletonApplication.java
@@ -0,0 +1,13 @@
+package com.skeleton.springsecurityskeleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySkeletonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecuritySkeletonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skeleton/springsecurityskeleton/SpringSecuritySkeletonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skeleton.springsecurityskeleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySkeletonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
